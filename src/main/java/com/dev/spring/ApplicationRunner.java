cf14c364457860d9f5cb2fdfca0bd05821689754
VladimirImir <mimimvvv@gmail.com>
2024-03-23 03:31:45 +0200

    Spring. 5. - XML-based Configuration.

--- src/main/java/com/dev/spring/ApplicationRunner.java
+++ src/main/java/com/dev/spring/ApplicationRunner.java
@@ -1,28 +1,15 @@
 package com.dev.spring;
 
 import com.dev.spring.database.pool.ConnectionPool;
-import com.dev.spring.database.repository.CompanyRepository;
-import com.dev.spring.database.repository.UserRepository;
-import com.dev.spring.ioc.Container;
-import com.dev.spring.service.UserService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
 
     public static void main(String[] args) {
 
-        var container = new Container();
-
-
-//        var connectionPool = new ConnectionPool();
-//        var userRepository = new UserRepository(connectionPool);
-//        var companyRepository = new CompanyRepository(connectionPool);
-//        var userService = new UserService(userRepository, companyRepository);
-
-//        var connectionPool = container.get(ConnectionPool.class);
-//        var userRepository = container.get(UserRepository.class);
-//        var companyRepository = container.get(CompanyRepository.class);
-        
-        var userService = container.get(UserService.class);
-        // TODO: userService
+        var context = new ClassPathXmlApplicationContext("application.xml");
+        // clazz -> String -> Map<String, Object>
+        var connectionPool = context.getBean("p1", ConnectionPool.class);
+        System.out.println(connectionPool);
     }
 }
