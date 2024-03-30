1d6ecf7d0dadd429f81d9b2c0d69f4acdde84f27
VladimirImir <mimimvvv@gmail.com>
2024-03-30 15:04:11 +0200

    Spring. 16. - Custom BeanPostProcessor. Часть 1.

--- src/main/java/com/dev/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/dev/spring/database/repository/CompanyRepository.java
@@ -1,16 +1,10 @@
 package com.dev.spring.database.repository;
 
+import com.dev.spring.bpp.InjectBean;
 import com.dev.spring.database.pool.ConnectionPool;
 
 public class CompanyRepository {
 
-    private final ConnectionPool connectionPool;
-
-    public CompanyRepository(ConnectionPool connectionPool) {
-        this.connectionPool = connectionPool;
-    }
-
-    public static CompanyRepository of(ConnectionPool connectionPool) {
-        return new CompanyRepository(connectionPool);
-    }
+    @InjectBean
+    private ConnectionPool connectionPool;
 }
