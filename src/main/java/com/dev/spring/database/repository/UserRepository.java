31fe6dba97b532f8ea004494cf510d58d1040169
VladimirImir <mimimvvv@gmail.com>
2024-04-01 14:33:43 +0300

    Spring. 19. - Classpath Scanning.

--- src/main/java/com/dev/spring/database/repository/UserRepository.java
+++ src/main/java/com/dev/spring/database/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.dev.spring.database.repository;
 
 import com.dev.spring.database.pool.ConnectionPool;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class UserRepository {
 
     private final ConnectionPool connectionPool;
--- src/main/java/com/dev/spring/service/UserService.java
+++ src/main/java/com/dev/spring/service/UserService.java
@@ -1,20 +1,20 @@
 package com.dev.spring.service;
 
+import com.dev.spring.database.entity.Company;
 import com.dev.spring.database.repository.CompanyRepository;
+import com.dev.spring.database.repository.CrudRepository;
 import com.dev.spring.database.repository.UserRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
 
     private final UserRepository userRepository;
-    private final CompanyRepository companyRepository;
-    private CompanyService companyService;
+    private final CrudRepository<Integer, Company> companyRepository;
 
-    public UserService(UserRepository userRepository, CompanyRepository companyRepository) {
+    public UserService(UserRepository userRepository,
+                       CrudRepository<Integer, Company> companyRepository) {
         this.userRepository = userRepository;
         this.companyRepository = companyRepository;
-    }
-
-    public void setCompanyService(CompanyService companyService) {
-        this.companyService = companyService;
     }
 }
