b5f8fa1a40bbda450787f1f05fc079a98877655a
VladimirImir <mimimvvv@gmail.com>
2024-04-03 19:16:52 +0300

    Spring. 22. - @Scope.

--- src/main/java/com/dev/spring/database/repository/UserRepository.java
+++ src/main/java/com/dev/spring/database/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.dev.spring.database.repository;
 
 import com.dev.spring.database.pool.ConnectionPool;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class UserRepository {
 
     private final ConnectionPool connectionPool;
