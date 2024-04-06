28a1664eacbfb5e15c58c0c7fc31dc0c526785bf
VladimirImir <mimimvvv@gmail.com>
2024-04-06 16:05:50 +0300

    Spring. 36. - Properties.

--- src/main/java/com/dev/spring/ApplicationRunner.java
+++ src/main/java/com/dev/spring/ApplicationRunner.java
@@ -2,7 +2,6 @@ package com.dev.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ApplicationRunner {
@@ -10,5 +9,6 @@ public class ApplicationRunner {
     public static void main(String[] args) {
         var context = SpringApplication.run(ApplicationRunner.class, args);
         System.out.println(context.getBeanDefinitionCount());
+        System.out.println(context.getBean("pool1"));
     }
 }
