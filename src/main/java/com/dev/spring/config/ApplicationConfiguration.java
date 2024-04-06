dabb9d8ae889b15e54bda72491ed54105b58ebcf
VladimirImir <mimimvvv@gmail.com>
2024-04-06 15:17:04 +0300

    Spring. 34. - @SpringBootApplication.

--- src/main/java/com/dev/spring/config/ApplicationConfiguration.java
+++ src/main/java/com/dev/spring/config/ApplicationConfiguration.java
@@ -1,26 +1,16 @@
 package com.dev.spring.config;
 
 import com.dev.spring.database.pool.ConnectionPool;
-import com.dev.spring.database.repository.CrudRepository;
+
 import com.dev.spring.database.repository.UserRepository;
 import com.dev.web.WebConfiguration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.*;
-import org.springframework.context.annotation.ComponentScan.Filter;
-import org.springframework.stereotype.Component;
 
 //@ImportResource("classpath:application.xml")
 @Import(WebConfiguration.class)
 @Configuration(proxyBeanMethods = true)
-@PropertySource("classpath:application.properties")
-@ComponentScan(basePackages = "com.dev.spring",
-        useDefaultFilters = false,
-        includeFilters = {
-                @Filter(type = FilterType.ANNOTATION, value = Component.class),
-                @Filter(type = FilterType.ASSIGNABLE_TYPE, value = CrudRepository.class),
-                @Filter(type = FilterType.REGEX, pattern = "com\\..+Repository")
-        })
 public class ApplicationConfiguration {
 
     @Bean("pool2")
