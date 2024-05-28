56a9fc57b07ef720d9840b89893b2d1f9f1dbe09
VladimirImir <mimimvvv@gmail.com>
2024-05-28 19:45:46 +0300

    Spring. 49. - @Transactional. TestContext.

---
+++ src/test/java/com/dev/spring/integration/database/repository/CompanyRepositoryTest.java
@@ -0,0 +1,43 @@
+package com.dev.spring.integration.database.repository;
+
+import com.dev.spring.database.entity.Company;
+import com.dev.spring.integration.annotation.IT;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@IT
+@RequiredArgsConstructor
+@Transactional
+//@Commit
+class CompanyRepositoryTest {
+
+    private final EntityManager entityManager;
+
+    @Test
+    void findById() {
+        var company = entityManager.find(Company.class, 1);
+        assertNotNull(company);
+        assertThat(company.getLocales()).hasSize(2);
+    }
+
+    @Test
+    void save() {
+        var company = Company.builder()
+                .name("Apple1")
+                .locales(Map.of(
+                        "ru", "Apple описание",
+                        "en", "Apple description"
+                ))
+                .build();
+        entityManager.persist(company);
+        assertNotNull(company.getId());
+    }
+}
