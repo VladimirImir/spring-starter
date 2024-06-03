ed59a4161bea0bbe8d6ea8973d612ce2a7df7239
VladimirImir <mimimvvv@gmail.com>
2024-06-03 15:56:12 +0300

    Spring. 56. - NamedQuery.

--- src/main/java/com/dev/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/dev/spring/database/repository/CompanyRepository.java
@@ -4,6 +4,7 @@ import com.dev.spring.database.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,7 +12,7 @@ import java.util.Optional;
 public interface CompanyRepository extends JpaRepository<Company, Integer> {
 
     // Optional, Entity, Future
-    Optional<Company> findByName(String name);
+    Optional<Company> findByName(@Param("name2") String name);
 
     // Collection, Stream (batch, close)
     List<Company> findAllByNameContainingIgnoreCase(String fragment);
