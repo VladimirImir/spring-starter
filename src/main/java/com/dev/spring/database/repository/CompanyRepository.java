dffc31c8455040a376c208935ffec4634e3f12a3
VladimirImir <mimimvvv@gmail.com>
2024-06-03 15:39:13 +0300

    Spring. 55. - PartTreeJpaQuery.

--- src/main/java/com/dev/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/dev/spring/database/repository/CompanyRepository.java
@@ -1,14 +1,19 @@
 package com.dev.spring.database.repository;
 
 import com.dev.spring.database.entity.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.Repository;
+
+import java.util.List;
 import java.util.Optional;
 
-public interface CompanyRepository extends CrudRepository<Company, Integer> {
+public interface CompanyRepository extends JpaRepository<Company, Integer> {
 
-    Optional<Company> findById(Integer id);
+    // Optional, Entity, Future
+    Optional<Company> findByName(String name);
 
-    void delete(Company entity);
+    // Collection, Stream (batch, close)
+    List<Company> findAllByNameContainingIgnoreCase(String fragment);
 
 }
