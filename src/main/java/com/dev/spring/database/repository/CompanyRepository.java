c950af27ae2813d614e293ad2444fc8023ba6377
VladimirImir <mimimvvv@gmail.com>
2024-06-03 15:14:27 +0300

    Spring. 54. - RepositoryQuery.

--- src/main/java/com/dev/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/dev/spring/database/repository/CompanyRepository.java
@@ -1,10 +1,11 @@
 package com.dev.spring.database.repository;
 
 import com.dev.spring.database.entity.Company;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.Repository;
 import java.util.Optional;
 
-public interface CompanyRepository extends Repository<Company, Integer> {
+public interface CompanyRepository extends CrudRepository<Company, Integer> {
 
     Optional<Company> findById(Integer id);
 
