0c064ecb3b5b4cfc0c9b0d337794dee362d56391
VladimirImir <mimimvvv@gmail.com>
2024-06-04 17:42:43 +0300

    Spring. 57. - @Query.

--- src/main/java/com/dev/spring/database/repository/UserRepository.java
+++ src/main/java/com/dev/spring/database/repository/UserRepository.java
@@ -1,17 +1,25 @@
 package com.dev.spring.database.repository;
 
+import com.dev.spring.database.entity.User;
 import com.dev.spring.database.pool.ConnectionPool;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-@Repository
-@Scope(BeanDefinition.SCOPE_PROTOTYPE)
-@RequiredArgsConstructor
-public class UserRepository {
+import java.util.List;
 
-    @Qualifier("pool2")
-    private final ConnectionPool connectionPool;
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("select u from User u " +
+            "where u.firstname like %:firstname% and u.lastname like %:lastname%")
+    List<User> findAllBy(String firstname, String lastname);
+
+    @Query(value = "SELECT u.* FROM users u WHERE u.username = :username",
+            nativeQuery = true)
+    List<User> findAllByUsername(String username);
 }
