edfd97e43c5a6bac7e73f463a1cc2eb6cd08f05f
VladimirImir <mimimvvv@gmail.com>
2024-06-05 18:06:52 +0300

    Spring. 58. - @Modifying.

--- src/main/java/com/dev/spring/database/repository/UserRepository.java
+++ src/main/java/com/dev/spring/database/repository/UserRepository.java
@@ -1,12 +1,9 @@
 package com.dev.spring.database.repository;
 
+import com.dev.spring.database.entity.Role;
 import com.dev.spring.database.entity.User;
-import com.dev.spring.database.pool.ConnectionPool;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.context.annotation.Scope;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -22,4 +19,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query(value = "SELECT u.* FROM users u WHERE u.username = :username",
             nativeQuery = true)
     List<User> findAllByUsername(String username);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("update User u " +
+            "set u.role = :role " +
+            "where u.id in (:ids)")
+    int updateRole(Role role, Long... ids);
 }
