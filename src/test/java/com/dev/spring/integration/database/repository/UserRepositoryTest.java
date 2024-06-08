3430de3e96525b93e576746919e239917c958cb7
VladimirImir <mimimvvv@gmail.com>
2024-06-08 22:03:24 +0300

    Spring. 61. - @EntityGraph.

--- src/test/java/com/dev/spring/integration/database/repository/UserRepositoryTest.java
+++ src/test/java/com/dev/spring/integration/database/repository/UserRepositoryTest.java
@@ -26,11 +26,11 @@ class UserRepositoryTest {
     void checkPageable() {
         var pageable = PageRequest.of(1, 2, Sort.by("id"));
         var slice = userRepository.findAllBy(pageable);
-        slice.forEach(user -> System.out.println(user.getId()));
+        slice.forEach(user -> System.out.println(user.getCompany().getName()));
 
         while (slice.hasNext()) {
             slice = userRepository.findAllBy(slice.nextPageable());
-            slice.forEach(user -> System.out.println(user.getId()));
+            slice.forEach(user -> System.out.println(user.getCompany().getName()));
         }
     }
 
