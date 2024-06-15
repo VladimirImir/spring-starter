7be3df15b3e439baca4888000f83689534f56499
VladimirImir <mimimvvv@gmail.com>
2024-06-15 23:38:35 +0300

    Spring. 68. - JDBC Starter.

--- src/main/java/com/dev/spring/database/repository/FilterUserRepositoryImpl.java
+++ src/main/java/com/dev/spring/database/repository/FilterUserRepositoryImpl.java
@@ -1,10 +1,13 @@
 package com.dev.spring.database.repository;
 
+import com.dev.spring.database.entity.Role;
 import com.dev.spring.database.entity.User;
 import com.dev.spring.database.querydsl.QPredicates;
+import com.dev.spring.dto.PersonalInfo;
 import com.dev.spring.dto.UserFilter;
 import com.querydsl.jpa.impl.JPAQuery;
 import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.persistence.EntityManager;
 import java.util.List;
@@ -14,7 +17,18 @@ import static com.dev.spring.database.entity.QUser.user;
 @RequiredArgsConstructor
 public class FilterUserRepositoryImpl implements FilterUserRepository {
 
+    private static final String FIND_BY_COMPANY_AND_ROLE = """
+        SELECT 
+            firstname,
+            lastname,
+            birth_date
+        FROM users
+        WHERE company_id = ?
+            AND role = ?
+        """;
+
     private final EntityManager entityManager;
+    private final JdbcTemplate jdbcTemplate;
 
     @Override
     public List<User> findAllByFilter(UserFilter filter) {
@@ -29,5 +43,15 @@ public class FilterUserRepositoryImpl implements FilterUserRepository {
                 .from(user)
                 .where(predicate)
                 .fetch();
+    }
+
+    @Override
+    public List<PersonalInfo> findAllByCompanyIdAndRole(Integer companyId, Role role) {
+        return jdbcTemplate.query(FIND_BY_COMPANY_AND_ROLE,
+                (rs, rowNum) -> new PersonalInfo(
+                        rs.getString("firstname"),
+                        rs.getString("lastname"),
+                        rs.getDate("birth_date").toLocalDate()
+                ), companyId, role.name());
     }
 }
