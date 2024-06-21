4dd96283694c6fee23b5b3f690e91208abc9499a
VladimirImir <mimimvvv@gmail.com>
2024-06-21 13:00:10 +0300

    Spring. 83. - CRUD. Service Layer.

--- src/main/java/com/dev/spring/http/controller/UserController.java
+++ src/main/java/com/dev/spring/http/controller/UserController.java
@@ -3,9 +3,12 @@ package com.dev.spring.http.controller;
 import com.dev.spring.dto.UserCreateEditDto;
 import com.dev.spring.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.web.servlet.ServletWebServerFactoryAutoConfiguration;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 @RequestMapping("users")
@@ -13,37 +16,47 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserService userService;
+    private final ServletWebServerFactoryAutoConfiguration servletWebServerFactoryAutoConfiguration;
 
     @GetMapping
     public String findAll(Model model) {
-        //model.addAttribute("users", userService.findAll());
         //model.addAttribute("users", userService.findAll(filter));
+        model.addAttribute("users", userService.findAll());
         return "user/users";
     }
 
     @GetMapping("/{id}")
     public String findById(@PathVariable("id") Long id, Model model) {
-        //model.addAttribute("user", userService.findById(id));
-        return "user/user";
+        return userService.findById(id)
+                .map(user -> {
+                    model.addAttribute("user", user);
+                    return "user/user";
+                })
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public String create(@ModelAttribute UserCreateEditDto user) {
-        //userService.create(user);
-        return "redirect:/users/" + 25;
+
+        return "redirect:/users/" + userService.create(user).getId();
     }
 
     //@PutMapping("/{id}")
     @PostMapping("/{id}/update")
     public String update(@PathVariable("id") Long id, @ModelAttribute UserCreateEditDto user) {
-        //userService.update(id, user);
-        return "redirect:/users/{id}";
+        return userService.update(id, user)
+                .map(it -> "redirect:/users/{id}")
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
     //@DeleteMapping("/{id}")
     @PostMapping("/{id}/delete")
     public String delete(@PathVariable("id") Long id) {
-        //userService.delete(id);
+        if (!userService.delete(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
         return "redirect:/users";
     }
 }
