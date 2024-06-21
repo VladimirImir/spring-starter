9c7d78fd9eb7ffe30a7e9499b0f360087ba812ca
VladimirImir <mimimvvv@gmail.com>
2024-06-21 07:41:36 +0300

    Spring. 80. - @ModelAttribute.

--- src/main/java/com/dev/spring/http/controller/GreetingController.java
+++ src/main/java/com/dev/spring/http/controller/GreetingController.java
@@ -1,25 +1,43 @@
 package com.dev.spring.http.controller;
 
+import com.dev.spring.database.entity.Role;
 import com.dev.spring.database.repository.CompanyRepository;
 import com.dev.spring.dto.UserReadDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/api/v1")
 @SessionAttributes({"user"})
 public class GreetingController {
 
-    @GetMapping("hello")
-    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request) {
-        modelAndView.setViewName("greeting/hello");
-        modelAndView.addObject("user", new UserReadDto(1L, "Ivan"));
+    @ModelAttribute("roles")
+    public List<Role> roles() {
+        return Arrays.asList(Role.values());
+    }
 
-        return modelAndView;
+    @GetMapping("hello")
+    public String hello(Model model,
+                        HttpServletRequest request,
+                        @ModelAttribute("userReadDto") UserReadDto userReadDto) {
+
+        model.addAttribute("user", new UserReadDto(1L, "Ivan"));
+
+        return "greeting/hello";
+    }
+
+    @GetMapping("bye")
+    public String bye(@SessionAttribute("user") UserReadDto user, Model model) {
+
+        return "greeting/bye";
     }
 
     @GetMapping("hello/{id}")
@@ -36,14 +54,4 @@ public class GreetingController {
 
         return modelAndView;
     }
-
-    @GetMapping("bye")
-    public ModelAndView bye(@SessionAttribute("user") UserReadDto user) {
-        var modelAndView = new ModelAndView();
-        modelAndView.setViewName("greeting/bye");
-
-        return modelAndView;
-    }
-
-
 }
