5c751e16e74997527170fefd68c6162ea7ec2fcb
VladimirImir <mimimvvv@gmail.com>
2024-06-21 06:39:21 +0300

    Spring. 78. - Parameters, Headers, Cookies.

--- src/main/java/com/dev/spring/http/controller/GreetingController.java
+++ src/main/java/com/dev/spring/http/controller/GreetingController.java
@@ -2,19 +2,26 @@ package com.dev.spring.http.controller;
 
 import com.dev.spring.database.repository.CompanyRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/api/v1")
 public class GreetingController {
 
-    @GetMapping("hello")
-    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request, CompanyRepository companyRepository) {
+    @GetMapping("hello/{id}")
+    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request,
+                              @RequestParam Integer age,
+                              @RequestHeader String accept,
+                              @CookieValue("JSESSIONID") String jsessionId,
+                              @PathVariable Integer id) {
+        var ageParamValue = request.getParameter("age");
+        var acceptHeader = request.getHeader("accept");
+        var cookies = request.getCookies();
+
         modelAndView.setViewName("greeting/hello");
 
         return modelAndView;
