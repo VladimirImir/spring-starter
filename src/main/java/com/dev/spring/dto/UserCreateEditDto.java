c278c2d2ff3403d68974c98f7c0350d67f17e8fa
VladimirImir <mimimvvv@gmail.com>
2024-06-22 03:04:26 +0300

    Spring. 84. - Spring MVC Testing.

--- src/main/java/com/dev/spring/dto/UserCreateEditDto.java
+++ src/main/java/com/dev/spring/dto/UserCreateEditDto.java
@@ -2,10 +2,12 @@ package com.dev.spring.dto;
 
 import com.dev.spring.database.entity.Role;
 import lombok.Value;
+import lombok.experimental.FieldNameConstants;
 
 import java.time.LocalDate;
 
 @Value
+@FieldNameConstants
 public class UserCreateEditDto {
 
     String username;
