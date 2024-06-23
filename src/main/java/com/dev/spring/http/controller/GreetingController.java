9fc415e79e9ffa76b901b44473fc3409ca2fffe1
VladimirImir <mimimvvv@gmail.com>
2024-06-23 14:40:04 +0300

    Spring. 86. - Thymeleaf Starter. Введение.

--- src/main/java/com/dev/spring/http/controller/GreetingController.java
+++ src/main/java/com/dev/spring/http/controller/GreetingController.java
@@ -29,7 +29,7 @@ public class GreetingController {
                         HttpServletRequest request,
                         @ModelAttribute("userReadDto") UserReadDto userReadDto) {
 
-        //model.addAttribute("user", new UserReadDto(1L, "Ivan"));
+        model.addAttribute("user", userReadDto);
 
         return "greeting/hello";
     }
