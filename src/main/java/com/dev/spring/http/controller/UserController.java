f6d48e50bc7e5ff8d195c9ab09fca1509df41a75
VladimirImir <mimimvvv@gmail.com>
2024-06-25 13:10:26 +0300

    Spring. 93. - @ControllerAdvice & @ExceptionHandler.

--- src/main/java/com/dev/spring/http/controller/UserController.java
+++ src/main/java/com/dev/spring/http/controller/UserController.java
@@ -10,6 +10,7 @@ import com.dev.spring.service.UserService;
 import com.dev.spring.validation.group.CreateAction;
 import com.dev.spring.validation.group.UpdateAction;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.web.servlet.ServletWebServerFactoryAutoConfiguration;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -22,9 +23,11 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import javax.validation.groups.Default;
 
+@Slf4j
 @Controller
 @RequestMapping("users")
 @RequiredArgsConstructor
@@ -32,6 +35,7 @@ public class UserController {
 
     private final UserService userService;
     private final CompanyService companyService;
+
 
     @GetMapping
     public String findAll(Model model, UserFilter filter, Pageable pageable) {
