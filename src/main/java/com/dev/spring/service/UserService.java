c4ea21dc6406f3b02ead2630802200bff4d58cc3
VladimirImir <mimimvvv@gmail.com>
2024-06-26 08:38:16 +0300

    Spring. 98. - Get Image.

--- src/main/java/com/dev/spring/service/UserService.java
+++ src/main/java/com/dev/spring/service/UserService.java
@@ -1,5 +1,6 @@
 package com.dev.spring.service;
 
+import com.dev.spring.database.entity.User;
 import com.dev.spring.database.querydsl.QPredicates;
 import com.dev.spring.database.repository.UserRepository;
 import com.dev.spring.dto.UserCreateEditDto;
@@ -13,6 +14,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -50,6 +52,13 @@ public class UserService {
     public Optional<UserReadDto> findById(Long id) {
         return userRepository.findById(id)
                 .map(userReadMapper::map);
+    }
+
+    public Optional<byte[]> findAvatar(Long id) {
+        return userRepository.findById(id)
+                .map(User::getImage)
+                .filter(StringUtils::hasText)
+                .flatMap(imageService::get);
     }
 
     @Transactional
